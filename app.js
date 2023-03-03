2f65985d2a6eecf12922444665331e7146d06384
saurabh sharma <saurabhcoded@gmail.com>
2023-03-03 23:39:56 +0530

    initial Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const app = require("express")();
+
+app.get("/names", (req, res) => {
+  res.send("Saurabh Sharma");
+});
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
