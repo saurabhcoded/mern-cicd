c672417cfc0835ec7e9a4b45a2c75e9ede9661ef
saurabh sharma <saurabhcoded@gmail.com>
2023-03-04 00:50:20 +0530

    Configured github Actions

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.get("/names", (req, res) => {
   res.send("Saurabh Sharma");
 });
+
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
