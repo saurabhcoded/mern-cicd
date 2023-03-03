b128055bcb71cc574cda6dcdf05de032266dbdfe
saurabh sharma <saurabhcoded@gmail.com>
2023-03-04 01:16:41 +0530

    Changes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname, "/client/build")));
 app.get("/names", (req, res) => {
   res.send("Saurabh Sharma web");
 });
-
+// change 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
